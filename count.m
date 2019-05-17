33375524e884641a34d56667bc256de24b07c443
lucaslc761 <48732524+lucaslc761@users.noreply.github.com>
2019-05-16 21:25:09 -0300

    Função count

---
+++ count.m
@@ -0,0 +1,24 @@
+## Author: 1810587 <1810587@OFBDT--039987>
+## Created: 2019-05-16
+
+
+
+function p = count(agrupamento, l)
+    
+    for j=1:l
+       pi=0;
+       for i=1:1000
+          if (agrupamento(i , 5)==l)
+          pi=+1;
+          else 
+          endif
+       p(l)=pi;
+       endfor
+    endfor
+    printf("populações: %d \n",  p);  
+    printf("\n");
+%teste
+%
+%teste
+%
+endfunction
