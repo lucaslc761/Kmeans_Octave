0e43dd767af15dd52b97ce1d69c537927410ef9a
lucaslc761 <48732524+lucaslc761@users.noreply.github.com>
2019-05-19 16:14:57 -0300

    Reclassificando com base nos novos centroides

--- kmeans.m
+++ kmeans.m
@@ -1,3 +1,4 @@
+
 pkg load geometry
 
 agrupamento = load ("agrupamento.dat");
@@ -8,57 +9,89 @@ centroide = load ("centroides.dat");
 
 %---------||---------||---------||---------||---------||---------||----------%
 p1=[];
-p2=0;
+p2=[];
 k = 0;
-j=1;
 soma=0;
 elbow = 0;
-vari = [];
-teste1 =[12,5];
-d_eulc = [];
 %---------||---------setando as classes iniciais---------||----------%
 
-centroide(1,5)=1;
-centroide(2,5)=2;
 
-#inserindo uma coluna de zeros no dataset
+#inserindo uma coluna de zeros no dataset que será a coluna da classificação.
 
 for i=1 :1000
   agrupamento(i,5)=0;
 endfor
 
 
-#coloquei o valor de 10 so para teste, mas vamos mudar
-#para que ele compare com o valor dos centroide 1 e 2 
-for k = 2 : 5
-    for i = 1 : m
-        d_geral=[];
-        for x = 1:k
-            dist1 = sqrt(distancePoints (agrupamento(i,1:4),centroide(x,1:4)));
-            d_geral(x) = dist1;
-            p1(x)=0;
-            %dist2 = sqrt(distancePoints (agrupamento(i,1:4),centroide(2,1:4)));
-        endfor
-        %verificar menor distancia
-        for t=1:k
-            
-            menor= min(d_geral);  
-            if( d_geral(t)==menor) 
-              agrupamento(i,5)=t;
-              p1(t)+=1;
-            endif
-        endfor
-      #printf("\n");
+%Fará a adição de um novo cluster ao final de todo o processo para cada rodada
+%com K clusters.
+for k = 2 : x
+    
+    %inicializa variavel da primeira contagem de elementos por classe.
+    for p = 1:k
+        p1(p)=0; 
+    endfor
+    %inicializa variavel da segunda contagem de elementos por classe.
+    for np = 1:k
+        p2(np)=0; 
     endfor
     
-    for x=1:k
-            for i=1:m
-              if (agrupamento(i,5)==x)
-                soma = soma +agrupamento(i,1:4);
-              endif
+    %inicia a verificação da distancia de cada elemento para os K clusters
+    %inserindos.
+    for i = 1 : m
+        d_geral=[];   
+        for h = 1:k
+            dist1 = sqrt(distancePoints (agrupamento(i,1:4),centroide(h,1:4)));
+            d_geral(h) = dist1;
+            menor= min(d_geral);
         endfor
+        
+        %verificar menor distancia
+        for t=1:k
+            if( d_geral(t)==menor) 
+                agrupamento(i,5)=t;
+                p1(t)+=1;
+            endif
+        endfor
+        
     endfor
-    novo_c = soma/(p1);
+ 
+%recalculando os centroides 
+    for b=1:k
+      soma(b,1:4) = 0;
+      novo_c(b,1:4) = 0;
+      
+        for f=1:m
+          if (agrupamento(f,5)==b)
+            v = agrupamento(f,1:4);
+            soma = soma + v;
+          endif
+        endfor
+        if (p1(b)>0)
+            centroide(b,1:4) = soma(b,:)/p1(b);
+        endif
+    endfor
+    
+    %recalculando classificação com os novos centroides dados os novos pontos 
+    for ni = 1 : m
+        d_geral=[];     
+        
+        for nh = 1:k
+            dist1 = sqrt(distancePoints (agrupamento(ni,1:4),centroide(nh,1:4)));
+            d_geral(nh) = dist1;
+            menor= min(d_geral);
+        endfor
+        
+        %verificar nova menor distancia
+        for nt=1:k
+            
+            if( d_geral(nt)==menor) 
+                agrupamento(ni,5)=nt;
+                p2(nt)+=1;
+            endif
+        endfor 
+    endfor
+    
 endfor
 
 
